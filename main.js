3b312e89bb77ba2ba0170c7cf04c27cf953f9301
jessica you <jessicaxytan@gmail.com>
2021-05-24 00:03:03 -0400

    commit tree dbe43733512670b0a424c30f312ebd2b10d44823

--- main.js
+++ main.js
@@ -4,7 +4,57 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
-
+document.addEventListener("DOMContentLoaded", () => {
+  // Add the .hidden class to the error modal in the HTML so it does not appear when the page first loads
+  // modal is the section of the error display that needs to be hidden
+  // console.log(modal) grabs modal error section
+  // hides the error message when page first loads
+  // Display the error modal by removing the .hidden class
+  // Display the server error message in the modal
+  const modal = document.querySelector("#modal")
+  // When a user clicks on an empty heart:
+  // Change the heart to a full heart
+  // need to select all of the hearts using querySelectorAll
+  const hearts = document.querySelectorAll("span.like-glyph")
+  hearts.forEach(function(heart) {
+    // make a server call
+    // add an eventlistener to each heart
+    heart.addEventListener("click", event => {
+      // Invoke mimicServerCall to simulate making a server request
+      mimicServerCall()
+      // when successful, change heart
+      // Add the .activated-heart class to make the heart appear red
+      // use promise that have .then() with 2 conditions we can use
+      .then(() => {
+        if (heart.innerHTML == EMPTY_HEART) {
+          heart.innerHTML = FULL_HEART
+          heart.className = "activated-heart"
+        //do something with our response
+        } else {
+          // if empty, make full, add new class and make heart appear red
+          // else if full, make empty
+          heart.innerHTML = EMPTY_HEART
+          heart.className = "like-glyph"
+        }
+      })
+      // When the "server" returns a failure status:
+      // Respond to the error using a .catch(() => {}) block after your .then(() => {}) block.
+      .catch(error => {
+        // Display the error modal by removing the .hidden class
+        modal.hidden = false
+        // Display the server error message in the modal
+        const modalMessage = document.querySelector("#modal-message")
+        modalMessage.innerHTML = error
+        // Use setTimeout to hide the modal after 3 seconds (add the .hidden class)
+        // When the "server" returns a success status:
+        setTimeout(() => {
+          // do this during time out
+          modal.hidden = true
+        }, 3000)
+        })
+      })
+    })
+  })
 
 
 //------------------------------------------------------------------------------
